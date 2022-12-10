b95f63d5b0a7fe91fee644133cbd67fdd2985039
JonHunt1995 <116927012+JonHunt1995@users.noreply.github.com>
2022-12-10 03:57:28 -0600

    Address

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 given_name = input("Given name: ")
-family_name = input("Family name: " )
+family_name = input("Family name: ")
 street_address = input("Street address: ")
 postal_code = input("City and postal code: ")
 print(given_name + " " + family_name)
 print(street_address)
 print(postal_code)
-
-
